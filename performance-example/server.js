b4748af773129ed5fd76390ff607b8ae70adb2ad
trX-X <tarinkumarpodavu4@gmail.com>
2022-02-08 23:44:27 +0530

    Worked with Cluster and PM2

---
+++ performance-example/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {}
+}
+
+app.get("/", (req, res) => {
+  res.send(`Performance example : ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`Ding Ding Ding!!! : ${process.pid}`);
+});
+console.log("Running server.js");
+console.log("Worker process started");
+app.listen(3000);
