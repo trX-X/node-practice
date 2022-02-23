7abf8c8df7b3c21e16edfd0a2495313ad4f7cb16
trX-X <tarinkumarpodavu4@gmail.com>
2022-02-23 22:30:51 +0530

    Learning Node security

---
+++ security-example/server.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(helmet());
+
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret is 42");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening to Port : ${PORT}..`);
+  });
